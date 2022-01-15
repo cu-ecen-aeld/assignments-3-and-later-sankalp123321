b7c0bb8584aa89a9133ce90fcb3fedcf8749d91c
sankalp <sankalp13392@gmail.com>
2022-01-14 22:36:10 -0700

    Chore: added comments.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,5 +1,8 @@
+# Setting the default directory location to pwd
 writefile=.
 writestr=""
+
+# If the argument count is no 2, error.
 if [ $# -ne 2 ]
 then
     echo "Invalid number of argumnets."
@@ -10,14 +13,15 @@ else
     writefile=$1
     writestr=$2
 fi
-
+# Create the file in the directory.
 touch ${writefile}
+# Check of the file/directory was created successfullly.
 if [ ! -f ${writefile} ]
 then
     echo "File not created, re run the command."
     exit 1
 fi
-
+# Populate the file.
 echo ${writestr} > ${writefile}
 
 
