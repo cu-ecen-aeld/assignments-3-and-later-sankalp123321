2c0de56a1fee5b89212d5913c35a6107839eb9b9
sankalp <sankalp13392@gmail.com>
2022-02-13 14:49:07 -0700

    chore : working but might have an issue in autotest

--- server/server.c
+++ server/server.c
@@ -13,25 +13,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <errno.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
 #include <netinet/in.h>
 
+void gracefulShutdonw()
+{
+    remove("/var/tmp/aesdsocketdata");
+    _exit(0);
+}
+
 int main(int argc, char* argv[])
 {
     printf("Server application.\n");
+    signal(SIGINT, gracefulShutdonw);
+    signal(SIGKILL, gracefulShutdonw);
+    signal(SIGSTOP, gracefulShutdonw);
+    signal(SIGTERM, gracefulShutdonw);
     
+    openlog("aesdsocket", LOG_CONS|LOG_PERROR|LOG_PID, LOG_USER);
+
+    int write_to_fd = open("/var/tmp/aesdsocketdata", O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXO);
+    if(write_to_fd < 0)
+    {
+        syslog(LOG_ERR, "open file: %s", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
     struct addrinfo s_addr, *new_addr;
     struct sockaddr_storage client_addr;
     memset(&s_addr, 0, sizeof(s_addr));
     s_addr.ai_family = AF_INET6;
     s_addr.ai_socktype = SOCK_STREAM;
     s_addr.ai_flags = AI_PASSIVE;
-    int ret = getaddrinfo(NULL, "57600", &s_addr, &new_addr);
+    int ret = getaddrinfo(NULL, "9000", &s_addr, &new_addr);
     if(ret != 0)
     {
-        printf("getaddrinfo error %s\n", gai_strerror(ret));
+        syslog(LOG_ERR, "getaddrinfo error %s\n", gai_strerror(ret));
+        close(write_to_fd);
         return EXIT_FAILURE;
     }
 
@@ -39,45 +63,192 @@ int main(int argc, char* argv[])
     int server_fd = socket(PF_INET6, SOCK_STREAM, 0);
     if(server_fd < 0)
     {
-        perror("socket");
+        // perror("socket");
+        syslog(LOG_ERR, "socket: %s", strerror(errno));
+        close(write_to_fd);
+        freeaddrinfo(new_addr);
+        return EXIT_FAILURE;
     }
 
-    printf("Binding the file descriptor[%d] to the address[%s].\n", server_fd, new_addr->ai_addr->sa_data);
+    printf("Binding the file descriptor[%d].\n", server_fd);
     int bind_ret = bind(server_fd, new_addr->ai_addr, new_addr->ai_addrlen);
     if(bind_ret < 0)
     {
-        perror("bind");
+        // perror("bind");
+        syslog(LOG_ERR, "bind: %s", strerror(errno));
+        close(write_to_fd);
+        close(server_fd);
+        freeaddrinfo(new_addr);
+        return EXIT_FAILURE;
     }
 
-    printf("Listening on the socket.\n");
-    int listen_ret = listen(server_fd, 10);
-    if(listen_ret < 0)
+    while (1)
     {
-        perror("listen");
-    } 
-    socklen_t add_size = sizeof(client_addr);
-    printf("Accpet new connection.\n");
-    int new_fd = accept(server_fd, (struct sockaddr*)&client_addr, &add_size);
-    if(new_fd < 0)
-    {
-        perror("accept");
-    } 
+        printf("Listening on the socket.\n");
+        int listen_ret = listen(server_fd, 10);
+        if(listen_ret < 0)
+        {
+            // perror("listen");
+            syslog(LOG_ERR, "listen: %s", strerror(errno));
+            close(write_to_fd);
+            close(server_fd);
+            freeaddrinfo(new_addr);
+            return EXIT_FAILURE;
+        } 
+        socklen_t add_size = sizeof(client_addr);
+        struct sockaddr* sck_addr = (struct sockaddr*)&client_addr;
+        printf("Accpet new connection.\n");
+        int new_fd = accept(server_fd, sck_addr, &add_size);
+        if(new_fd < 0)
+        {
+            // perror("accept");
+            syslog(LOG_ERR, "accept: %s", strerror(errno));
+            close(write_to_fd);
+            close(server_fd);
+            freeaddrinfo(new_addr);
+            return EXIT_FAILURE;
+        } 
+        syslog(LOG_INFO, "Accepted connection from %s\n", sck_addr->sa_data);
 
-    printf("New connection fd[%d]\n", new_fd);
-    uint8_t buffer[1024];
-    uint16_t buffer_size = sizeof(buffer)/sizeof(buffer[0]);
-    int ret_val = recv(new_fd, buffer, buffer_size, 0);
-    if(buffer_size == ret_val)
-    {
-        perror("recv");
+        printf("New connection fd[%d] %s\n", new_fd, sck_addr->sa_data);
+        uint8_t buffer[1024];
+        uint16_t buffer_size = sizeof(buffer)/sizeof(buffer[0]);
+        memset(buffer, 0, buffer_size);
+
+        uint8_t *buf = (uint8_t*)malloc(1024);
+        buffer_size = 1024;
+        uint16_t total_buf_size = 0, realloc_count = 1;
+        memset(buf, 0, buffer_size);
+        if(buf == NULL)
+        {
+            syslog(LOG_ERR, "ENOMEM");
+            close(write_to_fd);
+            close(server_fd);
+            freeaddrinfo(new_addr);
+            return EXIT_FAILURE;
+        }
+        while (1)
+        {
+            uint8_t found_a_packet = 0;
+            int ret_val = recv(new_fd, &buf[total_buf_size], buffer_size, 0);
+            if (!ret_val)
+            {
+                // No more data to recv
+                break;
+            }
+            else if((buffer_size == ret_val) || (buffer_size != ret_val))
+            {
+
+                printf("\nrecvd: %d bytes\n", ret_val);
+
+                for (size_t i = 0; i < ret_val; i++)
+                {
+                    printf("%c", buf[i]);
+                }
+                printf("\n");
+
+                size_t count = 0, write_size = 0, i = 0;
+                for (i = total_buf_size; i < (total_buf_size+ret_val); i++)
+                {
+                    write_size++;
+              
+                    if(buf[i] == '\n')
+                    {
+                        found_a_packet = 1;
+                        int ret = write(write_to_fd, buf, strlen((char*)buf));
+                        if(ret == strlen((char*)buf))
+                        {
+                            printf("success in writing %ld %ld.\n", count, strlen((char*)buf));
+                        }
+                        break;
+                    }
+                    found_a_packet = 0;
+                }
+
+                if(!found_a_packet)
+                {
+                    realloc_count++;
+                    buf = realloc(buf, realloc_count*buffer_size);
+                    total_buf_size += buffer_size;
+                    if(buf == NULL)
+                    {
+                        syslog(LOG_ERR, "ENOMEM");
+                        free(buf);
+                        close(write_to_fd);
+                        close(server_fd);
+                        freeaddrinfo(new_addr);
+                        return EXIT_FAILURE;
+                    }
+                    printf("Allocated more memory. %d\n", total_buf_size);
+                }
+                continue;
+            }
+            else if(ret_val < 0)
+            {
+                switch (errno)
+                {
+                case EINTR:
+                    continue;
+                    break;
+                case ENOTSOCK:
+                case ENOTCONN:
+                case EFAULT:
+                case EBADF:
+                case ECONNREFUSED:
+                    {
+                        syslog(LOG_ERR, "recv: %s", strerror(errno));
+                        free(buf);
+                        close(write_to_fd);
+                        close(server_fd);
+                        freeaddrinfo(new_addr);
+                        return EXIT_FAILURE;
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+
+        memset(buffer, 0, buffer_size);
+        lseek(write_to_fd, 0, SEEK_SET);
+        while (1)
+        {
+            uint8_t buffer[2048];
+            int read_Val = read(write_to_fd, buffer, 2048);
+            if(!read_Val)
+            {
+                break;
+            }
+            if(read_Val < 0)
+            {
+                syslog(LOG_ERR, "read: %s", strerror(errno));
+                free(buf);
+                close(write_to_fd);
+                close(server_fd);
+                freeaddrinfo(new_addr);
+                return EXIT_FAILURE;
+            }
+            printf("Sending: %d bytes\n", read_Val);
+            // const char* str = "This is super good stuff\n"; 
+            // memcpy(buffer, str, strlen(str));
+            int ret_val = send(new_fd, buffer, read_Val, 0);
+            if(buffer_size == ret_val)
+            {
+                // perror("send");
+                syslog(LOG_ERR, "send: %s", strerror(errno));
+                free(buf);
+                close(write_to_fd);
+                close(server_fd);
+                freeaddrinfo(new_addr);
+                return EXIT_FAILURE;
+            }
+            printf("send bytes: %d\n", ret_val);
+        }
+        free(buf);
+        close(new_fd);
     }
-    const char* str = "This is super good stuff\n"; 
-    memcpy(buffer, str, strlen(str));
-    ret_val = send(new_fd, buffer, buffer_size, 0);
-    if(buffer_size == ret_val)
-    {
-        perror("send");
-    }
+    close(write_to_fd);
     close(server_fd);
     freeaddrinfo(new_addr);
     return EXIT_SUCCESS;
